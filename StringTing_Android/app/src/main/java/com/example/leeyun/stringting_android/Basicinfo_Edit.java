2a5a6c692e8f3d3ffd2c4172c9beba75657ac80a
leeyun1533 <leeyun1533@naver.com>
2017-09-22 01:34:41 +0900

    5번 기본정보입력 나이부분 spinner로 교체

--- StringTing_Android/app/src/main/java/com/example/leeyun/stringting_android/Basicinfo_Edit.java
+++ StringTing_Android/app/src/main/java/com/example/leeyun/stringting_android/Basicinfo_Edit.java
@@ -2,6 +2,15 @@ package com.example.leeyun.stringting_android;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
+
+import org.w3c.dom.Text;
+
+import static com.example.leeyun.stringting_android.R.id.Spinner_age;
 
 /**
  * Created by leeyun on 2017. 9. 16..
@@ -12,6 +21,17 @@ public class Basicinfo_Edit extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.basicinfo_edit);
+
+
+        Spinner age = (Spinner)findViewById(Spinner_age);
+        ArrayAdapter adapter = ArrayAdapter.createFromResource(this, R.array.age, R.layout.spinner_item);
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        age.setAdapter(adapter);
+
+
+
+
     }
 
 }
